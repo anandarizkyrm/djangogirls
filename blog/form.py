4cc45a5c1f1678f0d4f4cb4eaaa290207d21ee6f
anandarizkyrm <rizky.pky@gmail.com>
2023-02-24 10:45:30 +0700

    add some features

--- blog/form.py
+++ blog/form.py
@@ -1,8 +1,60 @@
 from distutils.command.clean import clean
 from xml.dom import ValidationErr
 from django import forms
-
+from django.contrib.auth import authenticate, login, logout
 from .models import Category, Post, Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class ChangePasswordForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'last_name', 'first_name']
+
+        # exclude = ['password1', 'password2', 'password']
+
+
+class AuthForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise forms.ValidationError("Invalid username or password")
+        else:
+            login(self.request, user)
+        if not user.is_active:
+            raise forms.ValidationError("User is not active")
+
+        return self.cleaned_data
+
+
+class RegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class CommentForm(forms.ModelForm):
@@ -47,6 +99,16 @@ class FilterPostForm(forms.Form):
     search = forms.CharField(required=False)
     categories = forms.ModelMultipleChoiceField(
         queryset=Category.objects.all(), required=False)
+    date_from = forms.DateField(required=False,
+                                widget=forms.SelectDateWidget())
+    date_to = forms.DateField(required=False, widget=forms.SelectDateWidget())
+
+    def clean(self):
+        date_from = self.cleaned_data['date_from']
+        date_to = self.cleaned_data['date_to']
+        if date_from and date_to and date_from > date_to:
+            self.add_error('date_to', "Date to Must be after date from")
+            self.add_error("date_from", "Date From must be before Date To")
 
 
 class PostForm(forms.ModelForm):
@@ -57,35 +119,15 @@ class PostForm(forms.ModelForm):
         fields = ['title', 'text', 'categories']
         # exclude = ('author', 'created_date', 'published_date')
 
-    def clean(self):
+    def clean_categories(self):
         categories = self.cleaned_data.get('categories')
-        title = self.cleaned_data.get('title')
-        text = self.cleaned_data.get('text')
 
-        # category = "Sport"
-        param = "Robotic"
         print(categories)
-        for i in categories:
 
-            if i.title == "Sport" and "olahraga" not in text or i.title == "Sport" and "olahraga" not in title:
-                raise forms.ValidationError(
-                    "Text and title must include Olahraga for Sport Category")
-            elif i.title == param and param not in title:
-                return self.add_error('title', "Title must include  'Robotic'")
-                # return self.
-            # elif  i.title in "Sport" and "olahraga" not in text :
-            #     return self.add_error('text', "Text must include Olahraga")
-            # elif  i.title in "Sport" and "olahraga"  not in title:
-            #     return self.add_error('title', "Title must include Olahraga")
-
-        return self.cleaned_data
-
-        # print(categories, self.cleaned_data)
-
-    # def clean_categories(self):
-    #     categories = self.cleaned_data.get('categories')
-
-    #     return categories
+        if categories and len(categories) > 3:
+            return self.add_error('categories',
+                                  "Sorry Cannot have more than 3 categories")
+        return categories
 
     def clean_text(self):
         text = self.cleaned_data.get("text")
@@ -103,3 +145,30 @@ class PostForm(forms.ModelForm):
             return self.add_error('title', f"Title must not include {x}")
 
         return title
+
+    def clean(self):
+        categories = self.cleaned_data.get('categories')
+        title = self.cleaned_data.get('title')
+        text = self.cleaned_data.get('text')
+
+        # category = "Sport"
+        param = "Robotic"
+        if categories:
+            for i in categories:
+
+                if i.title == "Sport" and "olahraga" not in text or i.title == "Sport" and "olahraga" not in title:
+                    raise forms.ValidationError(
+                        "Text and title must include Olahraga for Sport Category"
+                    )
+                elif i.title == param and param not in title:
+                    return self.add_error('title',
+                                          "Title must include  'Robotic'")
+                    # return self.
+                # elif  i.title in "Sport" and "olahraga" not in text :
+                #     return self.add_error('text', "Text must include Olahraga")
+                # elif  i.title in "Sport" and "olahraga"  not in title:
+                #     return self.add_error('title', "Title must include Olahraga")
+
+        return self.cleaned_data
+
+        # print(categories, self.cleaned_data)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,74 @@
 from unicodedata import category
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import is_valid_path
 from django.utils import timezone
 from django.shortcuts import redirect
-from blog.form import CategoryForm, PostForm, CommentForm
-from .models import Category, Post
+from blog.form import CategoryForm, FilterPostForm, PostForm, CommentForm, AuthForm, RegisterForm, UserProfileForm
+from .models import Category, Comment, Post
 from django.db.models import Sum, Count
+from django.db.models import Q
 from django.contrib.auth import get_user_model
-from datetime import time
+from django.core.paginator import Paginator
+from django.contrib import messages
 
 User = get_user_model()
+
+
+def editprofile(request, id):
+    user = get_object_or_404(User, id=id)
+    form = UserProfileForm(request.POST or None, instance=user)
+    if request.user.id != id:
+        return HttpResponse("Cannot Edit User you Are not this user",
+                            status=401)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Success update your profile')
+            return redirect('editprofile', id)
+
+    return render(request, 'blog/editprofile.html', {
+        'form': form,
+        'user': user
+    })
+
+
+def userprofile(request, id):
+    user = get_object_or_404(User, id=id)
+    posts = Post.objects.filter(author=user).order_by('-created_date')
+    total_post = Post.objects.filter(author=user).aggregate(
+        total_post=Count('id'))
+
+    context = {
+        'posts': posts,
+        'total_post': total_post['total_post'],
+        'user': user
+    }
+
+    return render(request, 'blog/profile.html', context)
+
+
+def signin(request):
+    type: str = request.GET.get('type')
+
+    print(type)
+
+    if type == "register":
+        form = RegisterForm(request.POST or None)
+    else:
+        form = AuthForm(request, request.POST or None)
+
+    if request.method == 'POST':
+
+        if form.is_valid() and type != 'register':
+            return redirect('post_list')
+        elif form.is_valid() and type == 'register':
+            form.save()
+            messages.info(request, 'Success register Please Login')
+            return redirect('signin')
+
+    return render(request, 'blog/signin.html', {'form': form, 'type': type})
 
 
 # Create your views here.
@@ -20,9 +79,46 @@ def post_list(request):
     posts = Post.objects.all().order_by('-created_date')
     total_post = Post.objects.aggregate(total_post=Count('id'))
 
+    blog_posts = Post.objects.values(
+        'title', 'id', 'visits', 'created_date', 'author_id',
+        'author__username', 'author__last_name').order_by('-created_date')[:5]
+
+    print(blog_posts)
+
+    if request.method == 'POST':
+        form = FilterPostForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            categories = form.cleaned_data['categories']
+            date_from = form.cleaned_data['date_from']
+            date_to = form.cleaned_data['date_to']
+
+            if search and categories:
+                posts = Post.objects.filter(
+                    Q(title__icontains=search)
+                    | Q(categories__in=categories)).order_by('-created_date')
+            elif search:
+                posts = Post.objects.filter(
+                    Q(title__icontains=search)).order_by('-created_date')
+            elif categories:
+                posts = Post.objects.filter(
+                    Q(categories__in=categories)).order_by('-created_date')
+            elif date_from and date_to:
+                posts = Post.objects.filter(
+                    created_date__gte=date_from,
+                    created_date__lte=date_to).order_by('-created_date')
+    else:
+        form = FilterPostForm(request.POST)
+
+    posts_paginator = Paginator(posts, 1)  # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = posts_paginator.get_page(page_number)
+
     context = {
-        "posts": posts,
+        "posts": page_obj.object_list,
+        "page_obj": page_obj,
         "total_posts": total_post,
+        "form": form,
     }
     return render(request, 'blog/post_list.html', context)
 
@@ -31,7 +127,7 @@ def create_post(request):
 
     if request.method == "POST":
         form = PostForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and request.user.is_authenticated:
             post = form.save(commit=False)
             post.author = request.user
             # post.categories = request.POST('categories')
@@ -39,6 +135,8 @@ def create_post(request):
             post.save()
             form.save_m2m()
             return redirect('detail_post', id=post.id)
+        elif not request.user.is_authenticated:
+            return HttpResponse('You are not authenticated, Please log in')
     else:
         form = PostForm()
 
@@ -58,6 +156,15 @@ def create_category(request):
     return render(request, 'blog/create_category.html', {"form": form})
 
 
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST' and request.user == post.author:
+        post.delete()
+        return redirect('post_list')
+    else:
+        return HttpResponse(status=401)
+
+
 def get_post_by_category(request, category_id):
     posts = Post.objects.filter(categories=category_id)
     context = {"posts": posts}
@@ -69,6 +176,15 @@ def categories(request):
     categories = Category.objects.all()
     context = {"categories": categories}
     return render(request, 'blog/category_list.html', context)
+
+
+def delete_category(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('categories')
+    else:
+        return HttpResponse(status=405)
 
 
 def detail_post(request, id):
@@ -89,6 +205,14 @@ def detail_post(request, id):
     return render(request, 'blog/detail_post.html', context)
 
 
+def delete_comment(request, id):
+    comment = get_object_or_404(Comment, id=id)
+
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('detail_post', id=comment.post_id)
+
+
 def edit_post(request, id):
 
     post = get_object_or_404(Post, id=id)
@@ -99,6 +223,7 @@ def edit_post(request, id):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            form.save_m2m()
             return redirect('detail_post', id=post.id)
     else:
         form = PostForm(instance=post)
